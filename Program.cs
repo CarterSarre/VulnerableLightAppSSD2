cdc40cad8348fe42ee84ad0a03a54540ab0273d1
Carter Sarre <cartersarre7@gmail.com>
2024-10-25 18:17:57 -0400

    Deployed to Azure

--- Program.cs
+++ Program.cs
@@ -2,42 +2,46 @@ using System.Web;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Builder;
-using VulnerableWebApplication.VLAController;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.HttpLogging;
-using Microsoft.AspNetCore.HttpOverrides;
-using VulnerableWebApplication.VLAModel;
-using VulnerableWebApplication.VLAIdentity;
-using VulnerableWebApplication.MidlWare;
-using VulnerableWebApplication.TestCpu;
-using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
 using GraphQL.Types;
 using GraphQL;
-using System.Net.Sockets;
+using VulnerableWebApplication.VLAController;
+using VulnerableWebApplication.MidlWare;
+using VulnerableWebApplication.TestCpu;
+using VulnerableWebApplication.VLAIdentity;
+using VulnerableWebApplication.VLAModel;
 
-
-// Configuration du service 
-
+// Configuration of services
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddJsonFile($"appsettings{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
     .AddEnvironmentVariables();
 
-// Swagger
+// Swagger configuration
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API",
+        Version = "v1",
+        Description = "API documentation"
+    });
+});
+
 builder.Services.AddAntiforgery();
 
-// GraphQL
-
+// GraphQL services
 builder.Services.AddSingleton<IClientService, ClientService>();
 builder.Services.AddSingleton<ClientDetailsType>();
 builder.Services.AddSingleton<ClientQuery>();
 builder.Services.AddSingleton<ISchema, ClientDetailsSchema>();
 builder.Services.AddGraphQL(b => b.AddAutoSchema<ClientQuery>().AddSystemTextJson());
 
+// HTTP logging
 builder.Services.AddHttpLogging(logging =>
 {
     logging.LoggingFields = HttpLoggingFields.All;
@@ -47,63 +51,55 @@ builder.Services.AddHttpLogging(logging =>
     logging.CombineLogs = true;
 });
 
-// Configuration de l'application :
+// Application pipeline configuration
 var app = builder.Build();
+
 app.UseAntiforgery();
 app.UseMiddleware<XRealIPMiddleware>();
 app.UseMiddleware<ValidateJwtMiddleware>();
 app.UseHttpLogging();
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    c.RoutePrefix = "swagger";
+});
 
-
-// Variables :
-VLAIdentity.SetSecret(app.Configuration["Secret"]);
-VLAIdentity.SetLogFile(app.Configuration["LogFile"]);
-VLAController.SetLogFile(app.Configuration["LogFile"]);
-
-
-// Endpoints :
-
-app.MapGet("/", async (string? lang) => await Task.FromResult(VLAController.VulnerableHelloWorld(HttpUtility.UrlDecode(lang))));
-
-app.MapPost("/Login", [ProducesResponseType(StatusCodes.Status200OK)] async (HttpRequest request, [FromBody] VulnerableWebApplication.VLAModel.Creds login) => await Task.FromResult(VLAIdentity.VulnerableQuery(login.User, login.Passwd)).Result).WithOpenApi();
-
+// Endpoint declarations
+app.MapGet("/", async (string? lang) => await Task.FromResult(VLAController.VulnerableHelloWorld(HttpUtility.UrlDecode(lang)))).WithOpenApi();
+app.MapPost("/Login", [ProducesResponseType(StatusCodes.Status200OK)] async (HttpRequest request, [FromBody] Creds login) => await Task.FromResult(VLAIdentity.VulnerableQuery(login.User, login.Passwd)).Result).WithOpenApi();
 app.MapGet("/Contract", async (string i) => await Task.FromResult(VLAController.VulnerableXmlParser(HttpUtility.UrlDecode(i)))).WithOpenApi();
-
 app.MapGet("/LocalWebQuery", async (string? i) => await VLAController.VulnerableWebRequest(i)).WithOpenApi();
-
 app.MapGet("/Employee", async (string i) => await Task.FromResult(VLAController.VulnerableObjectReference(i))).WithOpenApi();
-
 app.MapGet("/NewEmployee", async (string i) => await Task.FromResult(VLAController.VulnerableDeserialize(HttpUtility.UrlDecode(i)))).WithOpenApi();
-
 app.MapGet("/LocalDNSResolver", async (string i) => await Task.FromResult(VLAController.VulnerableCmd(HttpUtility.UrlDecode(i)))).WithOpenApi();
-
-app.MapPatch("/Patch", async ([FromHeader(Name="X-Forwarded-For")] string h, [FromForm] IFormFile file) => await VLAController.VulnerableHandleFileUpload(file, h)).DisableAntiforgery().WithOpenApi();
+app.MapPatch("/Patch", async ([FromHeader(Name = "X-Forwarded-For")] string h, [FromForm] IFormFile file) => await VLAController.VulnerableHandleFileUpload(file, h)).DisableAntiforgery().WithOpenApi();
 
 app.UseGraphQL<ISchema>("/Client");
+app.UseGraphQLPlayground("/GraphQLUI", new GraphQL.Server.Ui.Playground.PlaygroundOptions { GraphQLEndPoint = "/Client", SubscriptionsEndPoint = "/Client" });
 
-app.UseGraphQLPlayground("/GraphQLUI", new GraphQL.Server.Ui.Playground.PlaygroundOptions{GraphQLEndPoint="/Client",SubscriptionsEndPoint="/Client"});
-
-
-// Arguments :
-
+// Handle CLI arguments for URL and testing
 string url = args.FirstOrDefault(arg => arg.StartsWith("--url="));
 string test = args.FirstOrDefault(arg => arg.StartsWith("--test"));
 
-if(!string.IsNullOrEmpty(test))
+if (!string.IsNullOrEmpty(test))
 {
     Console.WriteLine("Start CPU Testing");
     TestCpu.TestAffinity();
 }
 
-if (string.IsNullOrEmpty(url))
+if (!builder.Environment.IsProduction())
 {
-    app.Urls.Add("http://localhost:4000");
-    app.Urls.Add("https://localhost:3000");
+    if (string.IsNullOrEmpty(url))
+    {
+        app.Urls.Add("http://localhost:4000");
+        app.Urls.Add("https://localhost:3000");
+    }
+    else
+    {
+        app.Urls.Add(url.Substring("--url=".Length));
+    }
 }
-else app.Urls.Add(url.Substring("--url=".Length));
 
-// Lancement :
-
+// Launch the application
 app.Run();
